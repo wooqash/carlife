7788b528eafd8c6dfac869e9879fae1cb560ac7d
wooqash <wooqash.webdesign@gmail.com>
2020-07-23 05:09:44 +0200

    chore: added some logic and styles to handle menu clicks behaviors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import './sass/styles.scss';
-import * as test from './scripts/test-module';
+// import * as test from './scripts/test-module';
 
-console.log('hello world TS', test.default, 'holla amigos');
+const removeActiveClassFromMenu = (items: HTMLCollection): void => {
+    Array.from(items).forEach((item: Element) => item.classList.remove('active'));
+};
+
+const handleMenuLinks = (event: Event): void => {
+    event.preventDefault();
+    const link = event.target as HTMLAnchorElement;
+    const menuItems = link.parentElement?.parentElement?.children;
+    if (menuItems) {
+        removeActiveClassFromMenu(menuItems);
+    }
+    link.parentElement?.classList.add('active');
+};
+
+const menuLinks = document.querySelectorAll('.main-nav__link') as NodeListOf<HTMLAnchorElement>;
+Array.from(menuLinks).forEach((link: HTMLAnchorElement) => link.addEventListener('click', handleMenuLinks));
