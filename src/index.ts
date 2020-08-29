3cd0da36121b71d4ff05610dd783775fbd9a35a2
wooqash <wooqash.webdesign@gmail.com>
2020-08-29 07:50:55 +0200

    feat: added Dialog box component with styles

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import './sass/styles.scss';
 // import * as test from './scripts/test-module';
 import handleView from './scripts/spy-menu';
 
-import * as utils from './scripts/classes/utils';
+import Utils from './scripts/classes/utils';
+import Dialog from './scripts/classes/dialog';
 
 let prevScrollPos = window.pageYOffset;
+const body = document.body as HTMLBodyElement;
 const mainMenu = document.getElementById('MainMenu') as HTMLDivElement;
 const menuToggler = document.getElementById('MainMenuToggler') as HTMLButtonElement;
 const menuLinks = document.querySelectorAll('.main-nav__link') as NodeListOf<HTMLAnchorElement>;
+
+const collapseButtons = document.querySelectorAll('[data-toggle="collapse"]') as NodeListOf<HTMLAnchorElement>;
+const dialogOpenButtons = document.querySelectorAll('[data-dialog="dialog"]') as NodeListOf<HTMLAnchorElement>;
+const dialogContainers = document.querySelectorAll('.dialog__content') as NodeListOf<HTMLElement>;
 
 const removeActiveClassFromMenu = (items: HTMLCollection): void => {
     Array.from(items).forEach((item: Element) => item.classList.remove('active'));
@@ -39,13 +45,34 @@ const showHideElementsOnScroll = (): void => {
     const scrollTopButton = document.getElementById('ScrollTop') as HTMLButtonElement;
 
     if (prevScrollPos > currentScrollPos) {
-        utils.default.removeClass(header, 'hide');
-        utils.default.addClass(scrollTopButton, 'hide');
+        Utils.removeClass(header, 'hide');
+        Utils.addClass(scrollTopButton, 'hide');
     } else {
-        utils.default.addClass(header, 'hide');
-        utils.default.removeClass(scrollTopButton, 'hide');
+        Utils.addClass(header, 'hide');
+        Utils.removeClass(scrollTopButton, 'hide');
     }
     prevScrollPos = currentScrollPos;
+};
+
+const handleCollapse = (event: Event): void => {
+    event.preventDefault();
+    const link = event.target as HTMLAnchorElement;
+    const collapseId = link ? link.hash.substr(1) : null;
+    const collapseElem = collapseId ? document.getElementById(collapseId) : null;
+
+    if (collapseElem && collapseElem.style) {
+        collapseElem.style.maxHeight =
+            !collapseElem.style.maxHeight || collapseElem.style.maxHeight === '0px'
+                ? `${collapseElem.scrollHeight}px`
+                : '0';
+
+        collapseElem.style.maxWidth =
+            !collapseElem.style.maxWidth || collapseElem.style.maxWidth === '0px' ? '100%' : '0';
+
+        Utils.toggleClass(collapseElem, 'offer-card__details--show');
+    }
+
+    console.log(event, link);
 };
 
 Array.from(menuLinks).forEach((link: HTMLAnchorElement) => {
@@ -53,6 +80,21 @@ Array.from(menuLinks).forEach((link: HTMLAnchorElement) => {
     link.addEventListener('click', handleMenuLinks);
 });
 
+Array.from(collapseButtons).forEach((link: HTMLAnchorElement) => {
+    link.addEventListener('click', handleCollapse);
+});
+
 menuToggler.addEventListener('click', toggleMainMenu);
 
 window.addEventListener('scroll', showHideElementsOnScroll);
+
+// Dialog
+body.addEventListener('click', Dialog.close);
+
+Array.from(dialogOpenButtons).forEach((link: HTMLAnchorElement) => {
+    link.addEventListener('click', Dialog.open);
+});
+
+Array.from(dialogContainers).forEach((container: HTMLElement) => {
+    container.addEventListener('click', Dialog.handleCloseBtn);
+});
