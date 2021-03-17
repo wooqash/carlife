3fd3effa8f2863523d6a5185a71cc5b7b8e65799
wooqash <wooqash.webdesign@gmail.com>
2021-03-17 17:42:15 +0100

    fix: set tabindex attribute at email protected elements

--- src/scripts/classes/emailProtector.ts
+++ src/scripts/classes/emailProtector.ts
@@ -1,11 +1,12 @@
 export default class EmailProtector {
-    constructor(private container: HTMLParagraphElement) {}
+    constructor(private container: HTMLElement) {}
 
     render(userId: string, domain: string): void {
         const emailLink = document.createElement('a');
         const emailLinkText = document.createTextNode(`${userId} at ${domain}`);
         emailLink.appendChild(emailLinkText);
         emailLink.setAttribute('href', `mailto:${userId}@${domain}`);
+        emailLink.setAttribute('tabindex', '0');
         this.container.append(emailLink);
     }
 }
